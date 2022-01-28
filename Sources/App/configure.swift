f1d0ed7f1351a6ccc523df305a37c1a7c1612907
Mike JR <mikedalvik@ya.ru>
2022-01-28 16:27:37 +0300

    Add environments

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,7 +7,13 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+
+    switch app.environment {
+    case .production:
+        app.databases.use(.sqlite(.file("prod.sqlite")), as: .sqlite)
+    default:
+        app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    }
 
     app.migrations.add(CreateTodo())
     app.migrations.add(CreateGarment())
