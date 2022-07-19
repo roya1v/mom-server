e1562decec7f2aa67359309833d81ad20c97dde5
Mike JR <mikedalvik@ya.ru>
2022-07-19 20:27:38 +0200

    Migrate to PGSQL

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,5 @@
 import Fluent
-import FluentSQLiteDriver
+import FluentPostgresDriver
 import Vapor
 
 // configures your application
@@ -7,13 +7,13 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-
-    switch app.environment {
-    case .production:
-        app.databases.use(.sqlite(.file("prod.sqlite")), as: .sqlite)
-    default:
-        app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
-    }
+    app.databases.use(.postgres(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .psql)
 
     app.migrations.add(CreateTodo())
     app.migrations.add(CreateGarment())
