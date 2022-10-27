84479b68b543e007bfa21cf60094e7f7fad8acf2
Mike JR <mikedalvik@ya.ru>
2022-10-27 18:07:11 +0200

    Added images with S3

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -13,9 +13,15 @@ public func configure(_ app: Application) throws {
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
     ), as: .psql)
 
+    app.minio = MinioService(
+        host: Environment.get("MINIO_HOST") ?? "127.0.0.1",
+        accessKeyId: Environment.get("MINIO_ACCESS_KEY_ID") ?? "vapor_acces_key",
+        secretAccessKey: Environment.get("MINIO_SECRET_ACCESS_KEY") ?? "vapor_secret_acces")
+
     app.migrations.add(CreateGarment())
     app.migrations.add(CreateStorageLocation())
     app.migrations.add(CreateStorageEntity())
+    app.routes.defaultMaxBodySize = "10mb"
 
     try routes(app)
 }
